2102c992390a89bbbc512b4e493fb55fd981627e
Rens Greveling <rensgreveling@MacBook-Pro-van-Rens.local>
2016-10-06 14:59:53 +0200

    made photo collage rotate (thanks mentor) and also fixed some minor issues in the code

--- js/photos.js
+++ js/photos.js
@@ -1,9 +1,38 @@
-$('#grow').click(function () {
-      var right = $('#right');
-      right.show("fast");
-      var width =  parseInt(100 * right.width() /right.parent().width() + 10,10);
-      $('#right').animate({
-          "width": "+="+ width 
-      }, "slow");
-      $('#text').html(width);
-  });
+$(document).ready(function(){
+	/* copy loaded thumbnails into carousel */
+	$('.row .thumbnail').on('load', function() {
+
+	}).each(function(i) {
+
+		// if(this.complete) {
+			console.log('Added image')
+			var item = $('<div class="item"></div>');
+			var itemDiv = $(this).parents('div');
+			var title = $(this).parent('a').attr("title");
+
+			item.attr("title",title);
+			$(itemDiv.html()).appendTo(item);
+			item.appendTo('.carousel-inner'); 
+	    	if (i==0){ // set first item active
+	    		item.addClass('active');
+	    	}
+	    // }
+	});
+
+	/* activate the carousel */
+	$('#modalCarousel').carousel({interval:false});
+
+	/* change modal title when slide changes */
+	$('#modalCarousel').on('slid.bs.carousel', function () {
+		$('.modal-title').html($(this).find('.active').attr("title"));
+	})
+
+	/* when clicking a thumbnail */
+	$('.row .thumbnail').click(function(){
+		var idx = $(this).parents('div').index();
+		var id = parseInt(idx);
+  		$('#myModal').modal('show'); // show the modal
+    	$('#modalCarousel').carousel(id); // slide carousel to selected
+	});
+
+})
